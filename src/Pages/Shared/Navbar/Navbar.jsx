2d5106e0ba0b5c31c81c361c48cbff14bf7b0421
GGTuran <tayyabturan67@gmail.com>
2024-04-28 19:24:37 +0600

    working on menu page

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,23 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   const navLinks = (
     <>
-      <li>
-        <a>Item 1</a>
-      </li>
-      <li>
-        <a >Item 2</a>
-      </li>
-      <li>
-        <a>Item 3</a>
-      </li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/menu">Menu</Link></li>
+      
     </>
   );
 
   return (
-    <div className="navbar fixed z-10 text-white bg-black opacity-40 max-w-screen-xl ">
+    <div className="navbar fixed z-10 text-black bg-white opacity-30 max-w-screen-xl ">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className="btn btn-outline btn-primary lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -40,7 +36,7 @@ const Navbar = () => {
             {navLinks}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">Bistro Boss</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navLinks}</ul>
