98308df2ae4028381259e247b553a742c3e5cfd1
GGTuran <tayyabturan67@gmail.com>
2024-04-29 11:47:44 +0600

    using custom hooks

--- src/Pages/Home/Popular/PopularMenu.jsx
+++ src/Pages/Home/Popular/PopularMenu.jsx
@@ -1,37 +1,43 @@
-import { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
+// import { useEffect, useState } from "react";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../Hooks/useMenu";
 
 
 const PopularMenu = () => {
 
-    const [menu, setMenu] = useState([]);
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular');
 
-    useEffect(() =>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data =>{
-            const popularItems = data.filter(item => item.category === 'popular');
-            setMenu(popularItems)
-        })
-    } ,[])
+    // const [menu, setMenu] = useState([]);
+
+    // useEffect(() =>{
+    //     fetch('menu.json')
+    //     .then(res=>res.json())
+    //     .then(data =>{
+    //         const popularItems = data.filter(item => item.category === 'popular');
+    //         setMenu(popularItems)
+    //     })
+    // } ,[])
 
     return (
 
        
-        <section className="m-10">
+        <section className="m-10 flex flex-col justify-center items-center">
             <SectionTitle
             heading="From Our Menu"
             subHeading="Popular Items"
             ></SectionTitle>
             <div className="grid md:grid-cols-2 gap-5">
                 {
-                    menu.map(item=> <MenuItem
+                    popular.map(item=> <MenuItem
                     key={item._id}
                     item={item}
                     ></MenuItem>)
                 }
             </div>
+            <button className="btn  btn-outline border-0 border-b-4 mt-5">Order Now</button>
         </section>
     );
 };
