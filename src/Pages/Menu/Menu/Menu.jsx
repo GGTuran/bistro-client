5cb163fa1d661f732310f6098d7fc1a032d6e815
GGTuran <tayyabturan67@gmail.com>
2024-04-30 23:03:59 +0600

    need to end this

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,20 +1,28 @@
+/* eslint-disable no-unused-vars */
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from "../../../../assets/menu/banner3.jpg"
-import PopularMenu from "../../Home/Popular/PopularMenu";
+import useMenu from "../../../Hooks/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
+// import PopularMenu from "../../Home/Popular/PopularMenu";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter(item => item.category === 'dessert');
+  const soup = menu.filter(item => item.category === 'soup');
+  const salad = menu.filter(item => item.category === 'salad');
+  const pizza = menu.filter(item => item.category === 'pizza');
+  const offered = menu.filter(item => item.category === 'offered');
   return (
     <div>
       <Helmet>
         <title>Bistro | Menu</title>
       </Helmet>
       <Cover img={menuImg} title='our menu'  ></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover img={menuImg} title='our menu'  ></Cover>
-      <PopularMenu></PopularMenu>
-      <Cover img={menuImg} title='our menu'  ></Cover>
-      <PopularMenu></PopularMenu>
+      <SectionTitle subHeading='do not Miss' heading="Today's Offer"></SectionTitle>
+      <MenuCategory items={offered}></MenuCategory>
+
     </div>
   );
 };
--- src/Pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/Pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,8 +1,17 @@
+/* eslint-disable react/prop-types */
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
+const MenuCategory = ({items}) => {
     return (
         <div>
-            
+            <div className="grid md:grid-cols-2 gap-5">
+                {
+                    items.map(item=> <MenuItem
+                    key={item._id}
+                    item={item}
+                    ></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
